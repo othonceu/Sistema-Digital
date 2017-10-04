87cc3acbe488b8fa870e3844344f92f9a773b395
othonceu <othonceu7@gmail.com>
2017-10-04 12:31:44 -0300

    atualizar

--- PiscaLeds/PiscaLeds.v
+++ PiscaLeds/PiscaLeds.v
@@ -1,11 +1,46 @@
 module PiscaLeds(
-	input clk,
-	output led
+	input CLOCK_50,
+	input[3:0] KEY,
+	input [9:0]SW,
+	output [7:0] LEDG,
+	output [9:0] LEDR
+
 );
+reg[31:0] counter;
+reg[3:0] soma=1;
 
-assign led = clk;
-
+always @(posedge CLOCK_50)
+begin
+	if(counter<=50000000)
+	begin
+		counter<= counter+soma;
+	end
+	else
+	begin
+		counter<=0;
+		LEDR[0]<=~LEDR[0];
+	end
+		if(SW[0]==1)
+		begin
+			soma=10;
+		end
+		if(SW[1]==1)
+		begin
+			soma=30;
+		end
+		if(SW[2]==1)
+		begin
+			soma=60;
+		end
+	if(counter == 25000000)
+	begin 
+		LEDG <=~LEDG;
+	end
+		
+	
+end
 endmodule
+
 
 module testbench;
 	
@@ -25,3 +60,8 @@ module testbench;
   end
 
 endmodule
+
+
+
+
+
